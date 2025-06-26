fee82cd446c462429dcccf8e1e2e1d4e1bc361fe
Shatha Almamari <shatha.almamari99@gmail.com>
2025-06-26 14:38:32 +0400

    java basics

---
+++ helloworld.java
@@ -0,0 +1,11 @@
+public class helloworld {
+    public static void main(String[] args){
+        System.out.println("hello code creator");
+        System.out.println("My name is Shatha Almamari");
+        System.out.println("I am from Ibri");
+        System.out.println("I hold a Bachelor's degree in Software Engineering from the University of Technology and Applied Sciences in Ibri");
+        System.out.println("My date of birth 15/11/1999");
+        System.out.println("My skills:effective communication,time management,quick learning ability,programming languages");
+        System.out.println("shatha.almamari99@gmail.com);
+    }
+}
